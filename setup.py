95861f07d17986ae4ab6d2c8f039256b9c6548bd
Abhinav Bhatele <bhatele@cs.umd.edu>
2022-01-17 15:31:25 -0500

    add setup.py file for pip (#8)

---
+++ setup.py
@@ -0,0 +1,19 @@
+# Copyright 2022 Parallel Software and Systems Group, University of Maryland.
+# See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
+
+
+from setuptools import setup
+
+setup(
+    name="pipit",
+    version="0.0.1",
+    description="A Python library for analyzing parallel execution traces",
+    url="https://github.com/hpcgroup/pipit",
+    author="Abhinav Bhatele",
+    author_email="bhatele@cs.umd.edu",
+    classifiers=["Development Status :: 2 - Pre-Alpha"],
+    keywords="distributed computing, parallel computing",
+    packages=[],
+)
