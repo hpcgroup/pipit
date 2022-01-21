4a094b656ef7895877072f4395ecb6df8e88c363
Aditya Ranjan <adi.k.ranjan@gmail.com>
2022-01-21 16:39:20 -0500

    style changes

--- pipit/readers/otf2_reader.py
+++ pipit/readers/otf2_reader.py
@@ -10,24 +10,22 @@ import multiprocessing as mp
 import pipit.tracedata
 
 
-
 class OTF2Reader:
     """Reader for OTF2 trace files"""
 
     def __init__(self, dir_name):
-        self.dir_name = dir_name #directory of otf2 file being read
-
+        self.dir_name = dir_name  # directory of otf2 file being read
 
     # handles otf2 and _otf2 objects
     def fieldToVal(self, defField):
         fieldType = str(type(defField))
         if "otf2.definitions" in fieldType:
-            return fieldType[25:-2] + " " + str(getattr(defField, "_ref")) #reference id of the nested object
+            # reference id of the nested object
+            return fieldType[25:-2] + " " + str(getattr(defField, "_ref"))
         elif "_otf2.Events" in fieldType:
             return str(defField)
         else:
             return defField
-
 
     # handles different data types
     def handleData(self, data):
@@ -36,10 +34,10 @@ class OTF2Reader:
         elif isinstance(data, tuple):
             return tuple([self.fieldToVal(dataElement) for dataElement in data])
         elif isinstance(data, dict):
-            return {self.fieldToVal(dataKey): self.fieldToVal(dataValue) for dataKey, dataValue in data.items()}
+            return {self.fieldToVal(dataKey): self.fieldToVal(dataValue)
+                    for dataKey, dataValue in data.items()}
         else:
             return self.fieldToVal(data)
-
 
     # converts the fields of a definition object to a dictionary
     def fieldsToDict(self, defObject):
@@ -47,30 +45,31 @@ class OTF2Reader:
         for field in defObject._fields:
             fieldName = str(field.name)
             fieldsDict[fieldName] = self.handleData(getattr(defObject, fieldName))
-                
-        if len(fieldsDict) == 1: #collapse single dictionaries to a string
+
+        if len(fieldsDict) == 1:  # collapse single dictionaries to a string
             return list(fieldsDict.values())[0]
         else:
             return fieldsDict
-
 
     # serial events reader
     def events_reader(self, rank_size):
         with otf2.reader.open(self.dir_name) as trace:
             rank, size = rank_size[0], rank_size[1]
             locations = list(trace.definitions._locations)
+
             perProcess = math.floor(len(locations) / size)
             beginInt, endInt = int(rank * perProcess), int((rank + 1) * perProcess)
+
             timestamps, eventTypes, eventAttributes, names = [], [], [], []
             locs, locTypes, locGroups, locGroupTypes = [], [], [], []
 
-            #selects locations based on the rank
+            # selects locations based on the rank
             if rank == size - 1:
                 loc_events = list(trace.events(locations[beginInt:]).__iter__())
-            else:
+            elif len(locations[beginInt:endInt]) != 0:
                 loc_events = list(trace.events(locations[beginInt: endInt]).__iter__())
 
-            #iterating through events and processing them
+            # iterating through events and processing them
             for loc_event in loc_events:
                 loc, event = loc_event[0], loc_event[1]
 
@@ -90,76 +89,89 @@ class OTF2Reader:
                 timestamps.append(event.time)
 
                 if eventType != "Leave":
-                    attributesDict =  {}
+                    attributesDict = {}
                     for key, value in vars(event).items():
                         if value is not None and key != "time":
-                            attributesDict[self.fieldToVal(key)] = self.handleData(value)
+                            attributesDict[self.fieldToVal(key)] = self.handleData(
+                                                                   value)
                     eventAttributes.append(attributesDict)
                 else:
-                    eventAttributes.append(float("NaN")) #no need for duplicate attributes
+                    # no need for duplicate attributes
+                    eventAttributes.append(float("NaN"))
 
-            trace.close() #close event files
+            trace.close()  # close event files
 
-        #returns dictionary with all events and their fields
-        return {"Event": eventTypes, "Timestamp (ns)": timestamps, "Name": names, "Location ID": locs, "Location Type": locTypes,
-                "Location Group ID": locGroups, "Location Group Type": locGroupTypes, "Attributes": eventAttributes}
-
+        # returns dictionary with all events and their fields
+        return {"Event": eventTypes, "Timestamp (ns)": timestamps, "Name": names,
+                "Location ID": locs, "Location Type": locTypes,
+                "Location Group ID": locGroups, "Location Group Type": locGroupTypes,
+                "Attributes": eventAttributes}
 
     # writes the definitions to a Pandas DataFrame
     def defToDF(self, trace):
-        definitions, defIds, attributes = [], [], [] #ids are for objects stored in a reference registry
-        for key in vars(trace.definitions).keys(): #iterating through definition registry attributes
+        # ids are for objects stored in a reference registry
+        definitions, defIds, attributes = [], [], []
+
+        # iterating through definition registry attributes
+        for key in vars(trace.definitions).keys():
             defAttr = getattr(trace.definitions, str(key))
-            if key == "clock_properties": #only definition type that is not a registry
+            if key == "clock_properties":  # only definition type that is not a registry
                 defIds.append("")
                 definitions.append(str(type(defAttr))[25:-2])
-                attributes.append(self.fieldsToDict(defAttr)) #converts a definition object to a dictionary of its attributes
+                # converts a definition object to a dictionary of its attributes
+                attributes.append(self.fieldsToDict(defAttr))
             elif "otf2" not in key:
-                for defObject in defAttr.__iter__(): #iterate through registry elements
+                # iterate through registry elements
+                for defObject in defAttr.__iter__():
                     try:
                         defIds.append(defObject._ref)
-                    except:
+                    except Exception:
                         defIds.append("")
                     definitions.append(str(type(defObject))[25:-2])
                     attributes.append(self.fieldsToDict(defObject))
 
-        #return the definitions as a DataFrame
-        defDF = pd.DataFrame({"Definition": definitions, "ID": defIds, "Attributes": attributes})
-        
-        return defDF
+        # return the definitions as a DataFrame
+        defDF = pd.DataFrame({"Definition": definitions,
+                              "ID": defIds, "Attributes": attributes})
 
+        return defDF
 
     # writes the events to a Pandas DataFrame
     def eventsToDF(self):
-        #parallelizes the reading of events
+        # parallelizes the reading of events
         poolSize, pool = mp.cpu_count(), mp.Pool()
-        eventsDict = pool.map(self.events_reader, [(rank, poolSize) for rank in range(poolSize)])
+        eventsDict = pool.map(self.events_reader, [(
+                     rank, poolSize) for rank in range(poolSize)])
 
-        #combines results from each process
+        # combines results from each process
         for i in range(len(eventsDict) - 1):
             for key, value in eventsDict[0].items():
                 value.extend(eventsDict[1][key])
             del eventsDict[1]
         eventsDict = eventsDict[0]
 
-        #returns the events as a DataFrame
+        # returns the events as a DataFrame
         eventsDF = pd.DataFrame(eventsDict)
 
-        #cleaning up timestamps
-        clockProps = self.definitions.loc[self.definitions["Definition"] == "ClockProperties"]["Attributes"].values[0]
+        # cleaning up timestamps
+        clockProps = self.definitions.loc[self.definitions[
+                     "Definition"] == "ClockProperties"]["Attributes"].values[0]
         offset, resolution = clockProps["global_offset"], clockProps["timer_resolution"]
         eventsDF["Timestamp (ns)"] -= offset
         eventsDF["Timestamp (ns)"] *= ((10**9) / resolution)
 
-        #ensures the DataFrame is in order of increasing timestamp
-        eventsDF.sort_values(by = "Timestamp (ns)", axis = 0, ascending = True, inplace = True, ignore_index = True)
+        # ensures the DataFrame is in order of increasing timestamp
+        eventsDF.sort_values(by="Timestamp (ns)", axis=0,
+                             ascending=True, inplace=True, ignore_index=True)
 
-        #using categorical dtypes for memory optimization
-        eventsDF = eventsDF.astype({"Event": "category", "Name": "category", "Location ID": "category", "Location Type": "category",
-                                    "Location Group ID": "category", "Location Group Type": "category"})
+        # using categorical dtypes for memory optimization
+        eventsDF = eventsDF.astype({"Event": "category", "Name": "category",
+                                    "Location ID": "category",
+                                    "Location Type": "category",
+                                    "Location Group ID": "category",
+                                    "Location Group Type": "category"})
 
         return eventsDF
-
 
     # returns a tuple containing definitions and events
     def read(self):
