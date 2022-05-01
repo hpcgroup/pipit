d6126f358f33a377d8c68cde03df484d28e6b3d3
Abhinav Bhatele <bhatele@cs.umd.edu>
2022-05-01 15:24:36 -0400

    Basic structure of Trace class

---
+++ pipit/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2022 Parallel Software and Systems Group, University of Maryland.
+# See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
+
+from .trace import Trace
