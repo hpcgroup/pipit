d07c00ffecb660fe1ef590471677b25c7c410ddd
Abhinav Bhatele <bhatele@cs.umd.edu>
2022-05-01 20:43:39 -0400

    Basic structure of Trace class (#16)

---
+++ pipit/trace.py
@@ -0,0 +1,31 @@
+# Copyright 2022 Parallel Software and Systems Group, University of Maryland.
+# See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
+
+
+class Trace:
+    """A trace dataset is read into an object of this type, which includes one
+    or more dataframes.
+    """
+
+    def __init__(self, definitions, events):
+        """Create a new Trace object."""
+        self.definitions = definitions
+        self.events = events
+
+    @staticmethod
+    def from_otf2(dirname):
+        """Read an OTF2 trace into a new Trace object."""
+        # import this lazily to avoid circular dependencies
+        from .readers.otf2_reader import OTF2Reader
+
+        return OTF2Reader(dirname).read()
+
+    @staticmethod
+    def from_hpctoolkit(dirname):
+        """Read an HPCToolkit trace into a new Trace object."""
+        # import this lazily to avoid circular dependencies
+        from .readers.hpctoolkit_reader import HPCToolkitReader
+
+        return HPCToolkitReader(dirname).read()
