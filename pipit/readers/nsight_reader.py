4fc539f4eddc034b6246046bd940bf0b7660ac3f
Jordan <jmarry@terpmail.umd.edu>
2022-07-03 16:19:48 -0400

    Reformatted Nsight reader

--- pipit/readers/nsight_reader.py
+++ pipit/readers/nsight_reader.py
@@ -6,18 +6,20 @@
 import pandas as pd
 import pipit.trace
 
+
 class NSightReader:
     """Reader for NSight trace files"""
 
     def __init__(self, dir_name) -> None:
         self.dir_name = dir_name
         self.file_name = self.dir_name + "/trace.csv"
-        
+
     """
-    This read function directly takes in a csv of the trace report and 
+    This read function directly takes in a csv of the trace report and
     utilizes pandas to convert it from a csv into a dataframe.
     """
-    def read (self):
+
+    def read(self):
         file = self.file_name
         df = pd.read_csv(file)
         return pipit.trace.Trace(None, df)
