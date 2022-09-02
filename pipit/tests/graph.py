329ad048eb375e9284a6b9849ed8c5a0e474f68b
Jordan <jmarry@terpmail.umd.edu>
2022-09-02 09:22:24 -0400

    Testing for graph and nsight_reader

---
+++ pipit/tests/graph.py
@@ -0,0 +1,94 @@
+# Copyright 2022 Parallel Software and Systems Group, University of Maryland.
+# See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
+
+import pipit.graph as graph
+import pandas as pd
+
+call_graph = graph.Graph()
+
+dirname = "data/nbody-nvtx/test_reader.csv"
+
+df = pd.read_csv(dirname)
+
+root = graph.Node(df.iloc[0]['Name'], None)
+root.add_calling_contex_id(df.iloc[0]['RangeStack'])
+root.add_time(df.iloc[0]['Start (ns)'], df.iloc[0]['End (ns)'])
+
+call_graph.add_root(root)
+call_graph.add_to_map(root.calling_context_ids[0], root)
+
+node = graph.Node(df.iloc[1]['Name'], root)
+node.add_calling_contex_id(df.iloc[1]['RangeStack'])
+node.add_time(df.iloc[1]['Start (ns)'], df.iloc[1]['End (ns)'])
+
+call_graph.add_to_map(node.calling_context_ids[0], node)
+
+# Check Node 
+
+# Check name of nodes
+def test_node_name():
+    assert(root.name == 'main')
+    assert(node.name == 'bar')
+
+# Check add child function
+def test_add_child():    
+    root.add_child(node)
+    assert(root.get_child() == [node])
+    assert(root.children == [node])
+    assert(node.get_child() == [])
+    assert(node.children == [])
+    
+# Check add calling context id
+def test_calling_context():
+    assert(root.calling_context_ids == [':1'])
+    assert(node.calling_context_ids == [':1:2'])
+
+# Check time being added to the nodes
+def test_time():
+    assert(root.time == [0,12934])
+    assert(node.time == [5,1351])
+
+# Check level of nodes
+def test_level():
+    assert(root.get_level() == 0)
+    assert(node.get_level() == 1)
+    
+
+test_node_name()
+test_add_child()
+test_calling_context()
+test_time()
+
+# Check Graph
+
+# Test context map from graph with given inputs
+def test_context_map():
+    assert(call_graph.calling_context_id_map == {':1': root, ':1:2': node})
+    
+# Test if the call graph is correct with given input
+def test_root():
+    assert(call_graph.roots == [root])
+
+# Test if the the get node function works to retrieve the given input
+def test_get_node():
+    assert(call_graph.get_node(':1') == root)
+    assert(call_graph.get_node(':1:2') == node)
+
+# Test if the is empty function works on the call graph
+def test_is_empty():
+    assert(call_graph.is_empty() == False)
+    assert(call_graph.is_empty() != True)
+
+# Test if the graph output is correct
+def test_graph():
+    output = '\t' * 0 +'main\n'+ '\t'*1 + 'bar\n'
+        
+    assert(call_graph.get_graphs() == output)
+    
+test_context_map()
+test_root()
+test_get_node()
+test_is_empty()
+test_graph()
---
+++ pipit/tests/nsight_reader.py
@@ -0,0 +1,70 @@
+# Copyright 2022 Parallel Software and Systems Group, University of Maryland.
+# See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
+
+from pipit.readers.nsight_reader import NSightReader
+import pipit.trace as trace
+import pipit.graph as graph
+import pandas as pd
+from pandas.testing import assert_frame_equal
+
+dirname = "data/nbody-nvtx/test_reader.csv"
+
+cols = ['Name', 'RangeStack', 'Event Type', 'Time']
+
+data = pd.read_csv(dirname)
+
+# Check dataframe is loaded correctly
+def test_dataframe():
+    fun = NSightReader(dirname)
+    assert_frame_equal(fun.df, data)
+
+# Checking the Reader
+
+# Check if the reader does the correct work
+def test_len_reader():
+    fun = NSightReader(dirname)
+    assert(len(fun.read().events) == len(data)*2)
+    assert(len(fun.read().events.columns.values) == len(data.columns.values))
+
+# Test if the nsight reader made changes to the columns
+def test_reader_columns():
+    fun = NSightReader(dirname)
+    assert(cols == list(fun.read().events.columns.values))
+   
+# Test if the column changes happened or not 
+def test_column_changes():
+    fun = NSightReader(dirname)
+    assert(fun.df.columns.values is not fun.read().events.columns.values)
+    
+# Test of the dataframe is ascending by time
+def test_ascending_by_time():
+    fun = NSightReader(dirname)
+    assert(fun.read().events['Time'].is_monotonic_increasing)
+
+# Test if the reader output is the same as given
+def test_reader_output():
+    fun = NSightReader(dirname)
+    compare = [['main', ':1', 'Enter', 0], ['bar', ':1:2', 'Enter', 5], ['bar', ':1:2', 'Exit', 1351], ['main', ':1', 'Exit', 12934]]
+    df = pd.DataFrame(compare, columns=['Name', 'RangeStack', 'Event Type', 'Time'])
+    assert_frame_equal(df, fun.read().events)
+    
+test_dataframe()
+test_len_reader()
+test_reader_columns()
+test_ascending_by_time()
+test_column_changes()
+test_reader_output()
+
+# Checking the Call Graph
+
+# Check if create graph works correctly
+def test_graph():
+    fun = NSightReader(dirname)
+    
+    output = '\t' * 0 +'main\n'+ '\t'*1 + 'bar\n'
+        
+    assert(fun.call_graph().get_graphs() == output)
+    
+test_graph()
