ed6bef7fc610bf956fef1e50ea0985da3144cc34
Aditya Ranjan <adi.k.ranjan@gmail.com>
2022-09-08 18:21:57 -0400

    chore: sort hpctoolkit reader dataframe by timestamp (#28)

--- pipit/readers/hpctoolkit_reader.py
+++ pipit/readers/hpctoolkit_reader.py
@@ -303,5 +303,8 @@ class HPCToolkitReader:
                 close_node = close_node.parent
 
         trace_df = pd.DataFrame(data)
+        trace_df.sort_values(
+            by="Time", axis=0, ascending=True, inplace=True, ignore_index=True
+        )
         self.trace_df = trace_df
         return pipit.trace.Trace(None, trace_df)
