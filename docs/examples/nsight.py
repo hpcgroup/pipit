55a8d7213bbb8fc498713a55709a11ba4515767a
Jordan <jmarry@terpmail.umd.edu>
2022-09-14 19:57:37 -0400

    formatted nsight

--- docs/examples/nsight.py
+++ docs/examples/nsight.py
@@ -30,13 +30,13 @@ if __name__ == "__main__":
 
     # Use pipit's ``from_nsight`` API to read in the traces.
     # The result is stored into pipit's Trace data structure.
-    
+
     #
     trace = pp.Trace.from_nsight(dirname)
     #
 
     # Printout the DataFrame component of the Trace.
-    
+
     #
     print(trace.events)
     #
