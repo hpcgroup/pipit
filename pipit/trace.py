2739d66b3fe39091eda1820b46af703226c6f449
Jordan Marry <jmarry@terpmail.umd.edu>
2022-09-21 00:49:35 -0400

    WIP: time_profile code

--- pipit/trace.py
+++ pipit/trace.py
@@ -3,6 +3,9 @@
 #
 # SPDX-License-Identifier: MIT
 
+import pandas as pd
+import matplotlib.pyplot as plt
+
 
 class Trace:
     """A trace dataset is read into an object of this type, which includes one
@@ -29,3 +32,83 @@ class Trace:
         from .readers.hpctoolkit_reader import HPCToolkitReader
 
         return HPCToolkitReader(dirname).read()
+
+    @staticmethod
+    def from_nsight(dirname):
+        """Read an NSight trace into a new Trace object."""
+        # import this lazily to avoid circular dependencies
+        from .readers.nsight_reader import NSightReader
+
+        return NSightReader(dirname).read()
+
+    def binary(self, arr, low, high, x, start):
+        mid = (high + low) // 2
+        if high >= low:
+
+            if arr[mid] == x:
+                return mid
+
+            elif arr[mid] > x:
+                return self.binary(arr, low, mid - 1, x, start)
+
+            else:
+                return self.binary(arr, mid + 1, high, x, start)
+
+        else:
+            if start is True:
+                return mid
+            if start is False:
+                return mid + 1
+
+    # WIP
+
+    def time_profile(self, time_bins=100):
+        if time_bins <= 0:
+            return "Not Valid Amount of Bins"
+
+        # Start time and End time from trace file
+        start_time = self.events["Time"].min()
+        end_time = self.events["Time"].max()
+
+        bin_size = (end_time - start_time) / time_bins
+
+        intervals = [start_time + bin_size * interval for interval in range(time_bins)]
+
+        tp = {}
+
+        for i in range(0, len(self.events.index), 2):
+            # Get Function Name
+            func = self.events.iloc[i]["Name"]
+
+            tp[func] = [float(0)] * (len(intervals))
+
+        for i in range(0, len(self.events.index), 2):
+            # Find Start and End time for every function
+            start = self.events.iloc[i]["Time"]
+            end = self.events.iloc[i + 1]["Time"]
+            func = self.events.iloc[i]["Name"]
+
+            # Binary Search for index starting and index ending
+            start_bound = self.binary(intervals, 0, len(intervals) - 1, start, True)
+            end_bound = self.binary(intervals, 0, len(intervals) - 1, end, False)
+
+            # Time within
+            if start_bound == end_bound - 1:
+                tp[func][start_bound] += float(end - start)
+
+            else:
+                tp[func][start_bound] += intervals[start_bound + 1] - start
+                tp[func][start_bound + 1 : end_bound - 1] += bin_size
+                tp[func][end_bound - 1] += end - intervals[end_bound - 1]
+
+        tp_df = pd.DataFrame(tp)
+
+        tp_df["Bins"] = intervals
+
+        print(tp_df)
+
+        tp_df.plot.bar(x="Bins", stacked=True, title="Time in Functions")
+
+        plt.show()
+
+        return
