396bcd2b13beead32a808df5f7ee7b62e4c489eb
Jordan Marry <jmarry@terpmail.umd.edu>
2022-09-30 16:11:40 -0400

    Removed CCT code and updated PID and TID condition

--- pipit/readers/nsight_reader.py
+++ pipit/readers/nsight_reader.py
@@ -24,27 +24,17 @@ class NSightReader:
         # Read in csv
         self.df = pd.read_csv(self.file_name)
 
-        graph = self.create_cct()
-
         # Copy data into new dataframe
         df = self.df
         df2 = df.copy()
 
         # Create new columns for df with start time
-        df["Event Type"] = "Enter"
-        df["Time"] = df["Start (ns)"]
-
-        df["Graph_Node"] = df.apply(lambda x: graph.get_node(x["RangeStack"]), axis=1)
-        df["Level"] = df.apply(lambda x: graph.get_node(x["RangeStack"]).level, axis=1)
+        df["Event Type"] = "Entry"
+        df["Timestamp (ns)"] = df["Start (ns)"]
 
         # Create new columns for df2 with end time
         df2["Event Type"] = "Exit"
-        df2["Time"] = df2["End (ns)"]
-
-        df2["Graph_Node"] = df2.apply(lambda x: graph.get_node(x["RangeStack"]), axis=1)
-        df2["Level"] = df2.apply(
-            lambda x: graph.get_node(x["RangeStack"]).level, axis=1
-        )
+        df2["Timestamp (ns)"] = df2["End (ns)"]
 
         # Combine dataframes together
         df = pd.concat([df, df2])
@@ -52,102 +42,21 @@ class NSightReader:
         # Tidy Dataframe
         df.drop(["Start (ns)", "End (ns)"], axis=1, inplace=True)
 
-        df.sort_values(by="Time", ascending=True, inplace=True)
+        # Checks if multi-process trace
+        if len(set(df.loc[:, "PID"])) == 1:
+            df.drop(["PID"], axis=1, inplace=True)
+            # Checks if multi-threaded trace 
+            if len(set(df.loc[:, "TID"])) == 1:
+                df.drop(["TID"], axis=1, inplace=True)
+
+        # If multi-processed, checks if also multi-threaded
+        else:
+            if set(df.loc[:, "PID"]) == set(df.loc[:, "TID"]):
+                df.drop(["TID"], axis=1, inplace=True)
+
+
+        df.sort_values(by="Timestamp (ns)", ascending=True, inplace=True)
 
         df.reset_index(drop=True, inplace=True)
 
         return pipit.trace.Trace(None, df)
-
-    """
-    This create_cct function takes in a dataframe and shows the user
-    how the cct would look like within their program.
-
-    TODOS:
-    Fixed taking in mutliple roots, but context IDS will be weird
-    if range stack is the same for multiple roots
-    Add more Comments
-
-    """
-
-    def create_cct(self):
-        df = self.df
-
-        # Creating calling context graph
-        call_graph = graph.Graph()
-        callpath = dict()
-        stack, func_path = [], []
-        prev_rs_len, rs_len = 0, 0
-
-        for i in range(len(df)):
-            rs_len = len(df.iloc[i]["RangeStack"].split(":")) - 1
-
-            # If root
-            if rs_len == 1:
-                root = graph.Node(-1, df.iloc[i]["Name"], None)
-                root.add_calling_context_id(df.iloc[i]["RangeStack"])
-                root.add_time(df.iloc[i]["Start (ns)"], df.iloc[i]["End (ns)"])
-
-                # add root to tree
-                call_graph.add_root(root)
-                call_graph.add_to_map(root.calling_context_ids[0], root)
-
-                # add root stack
-                stack.append(df.iloc[i])
-                func_path.append(df.iloc[i]["Name"])
-
-                path = "->".join(func_path)
-
-                callpath[path] = root
-
-            # If child
-            elif rs_len > prev_rs_len:
-                path = "->".join(func_path)
-                func_path.append(df.iloc[i]["Name"])
-                path2 = "->".join(func_path)
-
-                if path2 not in callpath:
-                    parent = callpath[path]
-                    node = graph.Node(-1, df.iloc[i]["Name"], parent)
-                    node.add_calling_context_id(df.iloc[i]["RangeStack"])
-                    node.add_time(df.iloc[i]["Start (ns)"], df.iloc[i]["End (ns)"])
-                    parent.add_child(node)
-
-                    callpath[path2] = node
-                    call_graph.add_to_map(df.iloc[i]["RangeStack"], node)
-
-                else:
-                    callpath[path2].add_calling_context_id(df.iloc[i]["RangeStack"])
-                    call_graph.add_to_map(df.iloc[i]["RangeStack"], callpath[path2])
-
-                # Adding child function to stack
-                stack.append(df.iloc[i])
-
-            else:
-                for j in range(0, (prev_rs_len + 1) - rs_len):
-                    stack.pop()
-                    func_path.pop()
-
-                path = "->".join(func_path)
-                func_path.append(df.iloc[i]["Name"])
-                path2 = "->".join(func_path)
-
-                if path2 not in callpath:
-                    parent = callpath[path]
-                    node = graph.Node(-1, df.iloc[i]["Name"], parent)
-                    node.add_calling_context_id(df.iloc[i]["RangeStack"])
-                    node.add_time(df.iloc[i]["Start (ns)"], df.iloc[i]["End (ns)"])
-                    parent.add_child(node)
-
-                    callpath[path2] = node
-                    call_graph.add_to_map(df.iloc[i]["RangeStack"], node)
-
-                else:
-                    callpath[path2].add_calling_context_id(df.iloc[i]["RangeStack"])
-                    call_graph.add_to_map(df.iloc[i]["RangeStack"], callpath[path2])
-
-                # Adding child function to stack
-                stack.append(df.iloc[i])
-
-            prev_rs_len = rs_len
-
-        return call_graph
