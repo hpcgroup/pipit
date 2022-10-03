8a424a146fd21651e2b29433af9506eda98043b5
Jordan Marry <jmarry@terpmail.umd.edu>
2022-10-03 16:48:00 -0400

    Time Profile Code Updated. Needs work but should work correctly.

--- pipit/trace.py
+++ pipit/trace.py
@@ -3,9 +3,26 @@
 #
 # SPDX-License-Identifier: MIT
 
-import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
+
+
+class Node:
+    def __init__(self, name, pid, dur, graph_node) -> None:
+        self.name = name
+        self.pid = pid
+        self.dur = dur
+        self.graph_node = graph_node
+
+    def __str__(self) -> str:
+        return (
+            self.name
+            + ": "
+            + str(self.dur)
+            + " "
+            + str(self.pid)
+            + " "
+            + str(self.graph_node)
+        )
 
 
 class Trace:
@@ -129,53 +146,168 @@ class Trace:
 
     # WIP
 
-    def time_profile(self, time_bins=100):
-        if time_bins <= 0:
-            return "Not Valid Amount of Bins"
+    """
+    Time Profile for showing function times in time intervals
 
-        # Start time and End time from trace file
-        start_time = self.events["Time"].min()
-        end_time = self.events["Time"].max()
+    Arguments:
 
-        bin_size = (end_time - start_time) / time_bins
+    1) start_time (inclusive) -
+    Input an int to choose where you want the trace to start
 
-        intervals = [start_time + bin_size * interval for interval in range(time_bins)]
+    2) end time (exclusive) -
+    Input an int to choose where you want the trace to end
 
-        tp = {}
+    3) time_interval -
+    Input an int to choose how big you want the bins to be.
+    Recommended you stay at 1000000ns
+    Example Bin Size: 0-999999.9ns
 
-        for i in range(0, len(self.events.index), 2):
-            # Get Function Name
-            func = self.events.iloc[i]["Name"]
+    Returns:
+    A dictionary of arrays containing nodes containing the function
+    names, process id, duration in bin, and graph node.
 
-            tp[func] = [float(0)] * (len(intervals))
+    Issues:
+    1) Need to implement if start_time starts in the middle of a function
+    2) Need to optimize if possible.
+    """
 
-        for i in range(0, len(self.events.index), 2):
-            # Find Start and End time for every function
-            start = self.events.iloc[i]["Time"]
-            end = self.events.iloc[i + 1]["Time"]
-            func = self.events.iloc[i]["Name"]
+    def time_profile(self, start_time=0, end_time=245412000, time_interval=1000000):
+        if time_interval <= 0:
+            raise Exception("Not Valid Time Interval")
+        if start_time >= end_time or start_time < 0:
+            raise Exception("Invalid start/end time")
 
-            # Binary Search for index starting and index ending
-            start_bound = self.binary(intervals, 0, len(intervals) - 1, start, True)
-            end_bound = self.binary(intervals, 0, len(intervals) - 1, end, False)
+        # dict of bin times
+        # {bin interval times: array of nodes}
+        # Node contains name, pid, duration of how long in bin, graph node
+        bins = dict.fromkeys(
+            np.arange(start_time, end_time, time_interval).tolist(), []
+        )
 
-            # Time within
-            if start_bound == end_bound - 1:
-                tp[func][start_bound] += float(end - start)
+        # Add cushion for calculating duration time
+        bins[end_time + time_interval] = []
 
-            else:
-                tp[func][start_bound] += intervals[start_bound + 1] - start
-                tp[func][start_bound + 1 : end_bound - 1] += bin_size
-                tp[func][end_bound - 1] += end - intervals[end_bound - 1]
+        # Create a list of keys to use in binary search function
+        keys_list = list(bins.keys())
 
-        tp_df = pd.DataFrame(tp)
+        # Create a subset of events between the specific time frame
+        sub_main = self.events.loc[
+            (self.events["Time"] >= start_time) & (self.events["Time"] < end_time)
+        ]
 
-        tp_df["Bins"] = intervals
+        # Create a list of Different processes for filtering
+        process = sub_main["Process"].to_numpy()
+        process = np.unique(process).tolist()
 
-        print(tp_df)
+        for j in process:
+            # Create another subset of the specific process
+            sub = sub_main.loc[sub_main["Process"].isin([j])]
+            sub.reset_index(drop=True, inplace=True)
+            # dict of dicts
+            # {Function Name: {Graph_Node: Start Time}}
+            # Reason is that if a function has multiple calls
+            # with many different graph nodes, allows access in less time
+            stack = dict()
 
-        tp_df.plot.bar(x="Bins", stacked=True, title="Time in Functions")
+            # Going through the subset of certain process
+            for i in range(len(sub)):
+                data = sub.iloc[i]
+                # If the event type is enter, add it to the stack
+                if data["Event Type"] == "Enter":
+                    if data["Function Name"] not in stack:
+                        stack[data["Function Name"]] = {
+                            str(data["Graph_Node"]): data["Time"]
+                        }
+                    else:
+                        stack[data["Function Name"]][str(data["Graph_Node"])] = data[
+                            "Time"
+                        ]
+                # If the event type is exit, find the start time and
+                # end time of the certain function and add to the bins set
+                else:
+                    # else find start time in dicts of dicts
+                    # store start and end
+                    start_t = stack[data["Function Name"]][str(data["Graph_Node"])]
+                    end_t = data["Time"]
 
-        plt.show()
+                    # remove value from dict
+                    stack[data["Function Name"]].pop(str(data["Graph_Node"]))
 
-        return
+                    # Find start and end bins for where the nodes need to be added
+                    start_bound = self.binary(
+                        keys_list, 0, len(keys_list) - 1, start_t, True
+                    )
+                    end_bound = self.binary(
+                        keys_list, 0, len(keys_list) - 1, end_t, False
+                    )
+
+                    # If the function time fits in one bin
+                    if start_bound == end_bound - 1:
+                        bins[keys_list[start_bound]].append(
+                            Node(
+                                data["Function Name"],
+                                data["Process"],
+                                float(end_t - start_t) - 0.1,
+                                data["Graph_Node"],
+                            )
+                        )
+                    # if the function time spans across multiple bins with exits given
+                    else:
+                        bins[keys_list[start_bound]].append(
+                            Node(
+                                data["Function Name"],
+                                data["Process"],
+                                float(keys_list[start_bound + 1] - start_t) - 0.1,
+                                data["Graph_Node"],
+                            )
+                        )
+                        bins[keys_list[end_bound - 1]].append(
+                            Node(
+                                data["Function Name"],
+                                data["Process"],
+                                float(end_t - keys_list[end_bound - 1]),
+                                data["Graph_Node"],
+                            )
+                        )
+                        for i in range(start_bound + 1, end_bound - 1):
+                            bins[keys_list[i]].append(
+                                Node(
+                                    data["Function Name"],
+                                    data["Process"],
+                                    time_interval - 0.1,
+                                    data["Graph_Node"],
+                                )
+                            )
+
+            # if the stack has left over functions without any exits
+            if len(stack) > 0:
+                for k in stack:
+                    # check if function has events
+                    if len(stack[k]) > 0:
+                        inner_set = stack[k]
+                        for m in inner_set:
+                            # Find start time, and starting bin
+                            start_t = inner_set[m]
+                            start_bound = self.binary(
+                                keys_list, 0, len(keys_list) - 1, start_t, True
+                            )
+                            # add node to starting bin
+                            bins[keys_list[start_bound]].append(
+                                Node(
+                                    k,
+                                    j,
+                                    float(keys_list[start_bound + 1] - start_t) - 0.1,
+                                    m,
+                                )
+                            )
+                            # add nodes to the rest of the bins
+                            for i in range(start_bound + 1, len(keys_list)):
+                                bins[keys_list[i]].append(
+                                    Node(k, j, time_interval - 0.1, m)
+                                )
+
+        # remove buffer
+        bins.pop(end_time + time_interval)
+        print(stack)
+
+        return bins
