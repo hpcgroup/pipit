7e5b43a34f9741b73e1a3fdde40e78e6bab72c84
adityaranjan <adi.k.ranjan@gmail.com>
2022-10-15 22:12:26 -0400

    style: formatting changes

--- pipit/trace.py
+++ pipit/trace.py
@@ -17,7 +17,7 @@ class Trace:
         self.events = events
 
     @staticmethod
-    def from_otf2(dirname, num_parallel = None):
+    def from_otf2(dirname, num_parallel=None):
         """Read an OTF2 trace into a new Trace object."""
         # import this lazily to avoid circular dependencies
         from .readers.otf2_reader import OTF2Reader
