2f43df04fd66dd2c3fd89285ba7e6b09e63959aa
adityaranjan <adi.k.ranjan@gmail.com>
2022-11-09 23:45:09 -0500

    chore: add copyright

--- pipit/tests/trace.py
+++ pipit/tests/trace.py
@@ -1,3 +1,8 @@
+# Copyright 2022 Parallel Software and Systems Group, University of Maryland.
+# See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
+
 from pipit import Trace
 
 
