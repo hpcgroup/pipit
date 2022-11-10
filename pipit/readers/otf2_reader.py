10675a301142b61231547059f9cd3c20a5ae6077
adityaranjan <adi.k.ranjan@gmail.com>
2022-11-10 13:30:57 -0500

    docs: fixed typo in docs

--- pipit/readers/otf2_reader.py
+++ pipit/readers/otf2_reader.py
@@ -50,7 +50,7 @@ class OTF2Reader:
             Example: An event can have an attribute called region which corresponds
             to a definition. We strip the string and extract only the relevant
             information, which is the type of definition such as Region and also
-            append its id  (like Region 6) so that this definition ca be accessed
+            append its id  (like Region 6) so that this definition can be accessed
             in the Definitions DataFrame
             """
             return field_type[25:-2] + " " + str(getattr(field, "_ref"))
