012311a26ec0a672b90f5d777d07b4c5e460beee
Abhinav Bhatele <bhatele@cs.umd.edu>
2022-11-30 23:01:16 -0500

    change example

--- docs/examples/projections.py
+++ docs/examples/projections.py
@@ -5,7 +5,7 @@ import pipit as pp
 
 if __name__ == "__main__":
     # Path to OTF2 traces
-    dirname = "../../pipit/tests/data/ping-pong-projections/pingpong.prj"
+    dirname = "../../pipit/tests/data/ping-pong-projections"
 
     # Use pipit's ``from_projections`` API to read in the Projections traces.
     # The result is stored into pipit's Trace data structure.
