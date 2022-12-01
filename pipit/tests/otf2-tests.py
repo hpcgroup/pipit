3585a855d779c7c3bdf8b8cedf367f8894c31ae4
Abhinav Bhatele <bhatele@cs.umd.edu>
2022-12-01 00:12:39 -0500

    otf2 tests are allowed to fail

--- pipit/tests/otf2-tests.py
+++ pipit/tests/otf2-tests.py
@@ -6,7 +6,7 @@
 import numpy as np
 from pipit import Trace
 
-
+@pytest.mark.xfail(reason="Allow this to fail until otf2 has a pip package.")
 def test_events(data_dir, ping_pong_otf2_trace):
     trace = Trace.from_otf2(str(ping_pong_otf2_trace))
     events_df = trace.events
@@ -48,6 +48,7 @@ def test_events(data_dir, ping_pong_otf2_trace):
     assert (np.diff(events_df["Timestamp (ns)"]) > 0).all()
 
 
+@pytest.mark.xfail(reason="Allow this to fail until otf2 has a pip package.")
 def test_definitions(data_dir, ping_pong_otf2_trace):
     trace = Trace.from_otf2(str(ping_pong_otf2_trace))
     definitions_df = trace.definitions
