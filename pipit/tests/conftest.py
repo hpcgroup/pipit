a892b3a9e604fadf2d7bdb7054fd417f4297fc5d
Jordan <jmarry@terpmail.umd.edu>
2022-12-02 10:53:21 -0500

    added nsight unit tests

--- pipit/tests/conftest.py
+++ pipit/tests/conftest.py
@@ -40,3 +40,9 @@ def ping_pong_otf2_trace(data_dir, tmpdir):
     shutil.copy(os.path.join(str(otf2_dir), "traces.otf2"), str(tmpdir))
 
     return tmpdir
+
+@pytest.fixture
+def nbody_nvtx(data_dir, tmpdir):
+    nsight_dir = os.path.join(data_dir, "nbody-nvtx")
+    
+    shutil.copy(os.path.join(nsight_dir, "trace.csv"), str(tmpdir))
---
+++ pipit/tests/nsight-tests.py
@@ -0,0 +1,36 @@
+from pipit import Trace
+import pandas as pd
+
+def test_events(data_dir, nbody_nvtx):
+    events_df = Trace.from_nsight(str(nbody_nvtx)).events
+    
+    # 262 events in nbody trace
+    assert len(events_df) == 262
+    
+    # timestamp is increasing
+    assert pd.Index(events_df['Timestamp (ns)']).is_monotonic_increasing == True
+    
+    # event types for trace 
+    assert set(events_df['Event Type']) == set(['Enter, Leave'])
+    
+    # event names in trace
+    assert set(events_df['Name']) == set(
+        [
+            'main',
+            'cudaMallocManaged',
+            'cudaMemPrefechAsync',
+            'read_values_from_file',
+            'computeNBody11',
+            'bodyForce',
+            'cudaGetLastError',
+            'cudaDeviceSynchronize',
+            'integratePositions',
+            'computeNBody15',
+            'write_values_to_file'
+        ]
+    )
+    
+    # single process, single thread
+    assert len(events_df.loc[events_df['Name'] == 'main']) == 2
+    
+    assert events_df['PID'].equals(events_df['TID']) == True
