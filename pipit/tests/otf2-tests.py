b89fcd9231325bcd5b9106742265a7a87387720d
Abhinav Bhatele <bhatele@cs.umd.edu>
2022-12-01 20:51:41 -0500

    GitHub CI: use ubuntu-20.04 for Python 3.6 (#53)
    * use ubuntu-20.04 for Python 3.6
    
    * run pytest
    
    * otf2 tests are allowed to fail
    
    * remove otf2 import
    
    * add F821

--- pipit/tests/otf2-tests.py
+++ pipit/tests/otf2-tests.py
@@ -4,9 +4,11 @@
 # SPDX-License-Identifier: MIT
 
 import numpy as np
+import pytest
 from pipit import Trace
 
 
+@pytest.mark.xfail(reason="Allow this to fail until otf2 has a pip package.")
 def test_events(data_dir, ping_pong_otf2_trace):
     trace = Trace.from_otf2(str(ping_pong_otf2_trace))
     events_df = trace.events
@@ -48,6 +50,7 @@ def test_events(data_dir, ping_pong_otf2_trace):
     assert (np.diff(events_df["Timestamp (ns)"]) > 0).all()
 
 
+@pytest.mark.xfail(reason="Allow this to fail until otf2 has a pip package.")
 def test_definitions(data_dir, ping_pong_otf2_trace):
     trace = Trace.from_otf2(str(ping_pong_otf2_trace))
     definitions_df = trace.definitions
