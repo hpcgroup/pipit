ed54303eed5cb5c869dc592e71e960ca1194613a
Alexander Movsesyan <amovsesy@umd.edu>
2022-12-09 02:03:25 -0500

    added projections tests

---
+++ pipit/tests/projections-tests.py
@@ -0,0 +1,38 @@
+# Copyright 2022 Parallel Software and Systems Group, University of Maryland.
+# See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
+
+from pipit import Trace
+
+
+def test_events(data_dir, projections_directory):
+    trace = Trace.from_projections(str(projections_directory))
+    events_df = trace.events
+
+    # 108 total events in ping pong trace
+    assert len(events_df) == 1897
+
+    # The projections trace has 2 PEs
+    assert set(events_df["Process ID"]) == {0, 1}
+
+    # event types for trace (instant events are program begin/end and mpi send/recv)
+    assert set(events_df["Event Type"]) == {"Enter", "Instant", "Leave"}
+
+    # all event names in the trace
+    assert set(events_df["Name"]) == {
+        "Idle",
+        "Create",
+        "Processing",
+        "Computation",
+        "Unpack",
+        "Pack",
+    }
+
+    # 145 Create events
+    assert len(events_df.loc[events_df["Name"] == "Create"]) == 145
+
+    # Each log file starts/ends with a Computation Event
+    assert events_df.loc[events_df["Process ID"] == 1].loc[
+        events_df["Name"] == "Computation"
+    ]
