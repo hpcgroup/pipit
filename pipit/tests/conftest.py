3b8c9d12d3daec3ca108fe65e8862be217fae490
Jordan <jmarry@terpmail.umd.edu>
2022-12-09 12:21:59 -0500

    formatted code

--- pipit/tests/conftest.py
+++ pipit/tests/conftest.py
@@ -41,10 +41,11 @@ def ping_pong_otf2_trace(data_dir, tmpdir):
 
     return tmpdir
 
+
 @pytest.fixture
 def nbody_nvtx(data_dir, tmpdir):
     nsight_dir = os.path.join(data_dir, "nbody-nvtx")
-    
+
     shutil.copy(os.path.join(nsight_dir, "trace.csv"), str(tmpdir))
-    
+
     return tmpdir
--- pipit/tests/nsight-tests.py
+++ pipit/tests/nsight-tests.py
@@ -1,37 +1,37 @@
 from pipit import Trace
 import pandas as pd
-import pytest
+
 
 def test_events(nbody_nvtx):
-    events_df = Trace.from_nsight(str(nbody_nvtx)+"/trace.csv").events
-    
+    events_df = Trace.from_nsight(str(nbody_nvtx) + "/trace.csv").events
+
     # 262 events in nbody trace
     assert len(events_df) == 262
-    
+
     # timestamp is increasing
-    assert pd.Index(events_df['Timestamp (ns)']).is_monotonic_increasing == True
-    
-    # event types for trace 
-    assert set(events_df['Event Type']) == set(['Enter', 'Leave'])
-    
+    assert pd.Index(events_df["Timestamp (ns)"]).is_monotonic_increasing is True
+
+    # event types for trace
+    assert set(events_df["Event Type"]) == set(["Enter", "Leave"])
+
     # event names in trace
-    assert set(events_df['Name']) == set(
+    assert set(events_df["Name"]) == set(
         [
-            'main',
-            'cudaMallocManaged',
-            'cudaMemPrefechAsync',
-            'read_values_from_file',
-            'computeNBody11',
-            'bodyForce',
-            'cudaGetLastError',
-            'cudaDeviceSynchronize',
-            'integratePositions',
-            'computeNBody15',
-            'write_values_to_file'
+            "main",
+            "cudaMallocManaged",
+            "cudaMemPrefechAsync",
+            "read_values_from_file",
+            "computeNBody11",
+            "bodyForce",
+            "cudaGetLastError",
+            "cudaDeviceSynchronize",
+            "integratePositions",
+            "computeNBody15",
+            "write_values_to_file",
         ]
     )
-    
+
     # single process, single thread
-    assert len(events_df.loc[events_df['Name'] == 'main']) == 2
-    
-    assert events_df['PID'].equals(events_df['TID']) == True
+    assert len(events_df.loc[events_df["Name"] == "main"]) == 2
+
+    assert events_df["PID"].equals(events_df["TID"]) is True
