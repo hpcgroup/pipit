173041ced24b5c4c5717c89129408a86e5c08ae7
Rakrish Dhakal <dhakal.rakrish@gmail.com>
2023-02-21 00:59:33 -0500

    Add plot_timeline

--- pipit/vis/__init__.py
+++ pipit/vis/__init__.py
@@ -1,2 +1,2 @@
-from .core import plot_comm_matrix  # noqa: F401
+from .core import plot_comm_matrix, plot_timeline  # noqa: F401
 from ._util import reload_vis  # noqa: F401
--- pipit/vis/core.py
+++ pipit/vis/core.py
@@ -1,77 +1,195 @@
 import numpy as np
-from bokeh.models import AdaptiveTicker, HoverTool, NumeralTickFormatter
+import pandas as pd
+import pipit as pp
+
+from bokeh.io import show, output_notebook
+from bokeh.plotting import figure
+from bokeh.transform import factor_cmap
+from bokeh.models import FactorRange, RangeTool
+from bokeh.layouts import column
+
 
 from ._util import (
-    clamp,
-    format_size,
-    init_vis,
-    plot,
     process_tick_formatter,
-    size_hover_formatter,
-    size_tick_formatter,
+    time_tick_formatter,
 )
 
+output_notebook()
 
-def plot_comm_matrix(comm_matrix, type="size", label_threshold=16):
+
+def plot_timeline(trace):
+    """Plots summary timeline of events in a Trace."""
+    df = trace.events.copy(deep=False).reset_index()
+
+    df["Process"] = "Process " + df["Process"].astype("str")
+    df["Depth"] = "Depth " + df["Depth"].astype("int").astype("str")
+    df["y"] = df[["Process", "Depth"]].apply(lambda x: (x[0], str(x[1])), axis=1)
+
+    index_cmap = factor_cmap(
+        "Name",
+        palette=pp.config["vis"]["colors"],
+        factors=sorted(df.Name.unique()),
+        end=1,
+    )
+
+    p = figure(
+        output_backend="webgl",
+        y_range=FactorRange(*sorted(df["y"].unique(), reverse=True)),
+        sizing_mode="stretch_width",
+        height=len(df["y"].unique()) * 80 + 50,
+        title="Events Timeline",
+        tools=["xpan", "xwheel_zoom", "hover"],
+        x_range=trace.x_range,
+        x_axis_location="above",
+    )
+
+    print(p.x_range)
+
+    p.hbar(
+        y="y",
+        left="Timestamp (ns)",
+        right="Matching Timestamp",
+        height=1,
+        source=df[df["Event Type"] == "Enter"],
+        fill_color=index_cmap,
+        line_color="black",
+        line_width=0.5,
+        fill_alpha=1,
+    )
+
+    sends = df[df["Name"] == "MpiSend"]
+    recvs = df[df["Name"] == "MpiRecv"]
+
+    comm = pd.DataFrame()
+    comm["x0"] = sends["Timestamp (ns)"].values
+    comm["y0"] = sends["y"].values
+    comm["x1"] = recvs["Timestamp (ns)"].values
+    comm["y1"] = recvs["y"].values
+    comm["cx0"] = comm["x0"] + (comm["x1"] - comm["x0"]) * 0.5
+    comm["cx1"] = comm["x1"] - (comm["x1"] - comm["x0"]) * 0.5
+
+    p.bezier(
+        x0="x0",
+        y0="y0",
+        x1="x1",
+        y1="y1",
+        cx0="cx0",
+        cy0="y0",
+        cx1="cx1",
+        cy1="y1",
+        source=comm,
+        line_width=1,
+        line_color="black",
+    )
+
+    p.diamond(
+        x="Timestamp (ns)",
+        y="y",
+        source=df[df["Event Type"] == "Instant"],
+        size=12,
+        color="MediumAquaMarine",
+        line_width=1,
+        line_color="black",
+    )
+
+    p.xaxis.formatter = time_tick_formatter
+    p.y_range.range_padding = 0.5
+    p.yaxis.group_label_orientation = 0
+    p.yaxis.major_label_text_font_size = "0pt"
+    p.ygrid.grid_line_color = None
+
+    select = figure(
+        title="Drag the middle and edges of the selection box to change the range",
+        height=130,
+        sizing_mode="stretch_width",
+        y_range=p.y_range,
+        y_axis_type=None,
+        tools="",
+        toolbar_location=None,
+        background_fill_color="#efefef",
+    )
+
+    range_tool = RangeTool(x_range=p.x_range)
+    range_tool.overlay.fill_color = "navy"
+    range_tool.overlay.fill_alpha = 0.2
+
+    select.hbar(
+        y="Process",
+        left="Timestamp (ns)",
+        right="Matching Timestamp",
+        height=2,
+        source=df[df["Event Type"] == "Enter"],
+        fill_color=index_cmap,
+        line_color="black",
+        line_width=0.5,
+        fill_alpha=1,
+    )
+    select.xaxis.formatter = time_tick_formatter
+    select.ygrid.grid_line_color = None
+    select.add_tools(range_tool)
+    select.toolbar.active_multi = range_tool
+
+    show(column(p, select, sizing_mode="stretch_width"))
+
+
+def plot_comm_matrix(trace, type="size", label_threshold=16):
     """Plots heatmap of process-to-process message volume."""
-    import holoviews as hv
+    from bokeh.plotting import figure, show
+    from bokeh.models import (
+        AdaptiveTicker,
+        ColorBar,
+        LinearColorMapper,
+        PrintfTickFormatter,
+        BasicTicker,
+    )
+    from bokeh.palettes import YlGnBu9
 
-    init_vis()
-
+    comm_matrix = trace.comm_matrix()
     N = comm_matrix.shape[0]
-    bounds = (-0.5, -0.5, N - 0.5, N - 0.5)
 
-    # Generate image
-    image = hv.Image(np.flip(comm_matrix, 0), bounds=bounds).opts(
-        # width=clamp(300 + N * 35, 400, 850),
-        height=clamp(250 + N * 25, 200, 650),
-        # responsive=False,
-        colorbar=True,
-        colorbar_position="bottom",
-        cmap="YlOrRd",
-        tools=[
-            HoverTool(
-                tooltips={
-                    "Sender": "Process $x{0.}",
-                    "Receiver": "Process $y{0.}",
-                    "Bytes": "@image{custom}",
-                },
-                formatters={"@image": size_hover_formatter},
-            )
-        ],
-        xlabel="Sender",
-        ylabel="Receiver",
-        xticks=AdaptiveTicker(base=2, min_interval=1),
-        yticks=AdaptiveTicker(base=2, min_interval=1),
-        title="Process-to-process message volume",
-        yformatter=process_tick_formatter,
-        xformatter=process_tick_formatter,
-        xaxis="top",
-        invert_yaxis=True,
-        xrotation=60,
-        cformatter=size_tick_formatter if type == "size" else NumeralTickFormatter(),
+    mapper = LinearColorMapper(
+        palette=YlGnBu9, low=np.amin(comm_matrix), high=np.amin(comm_matrix)
     )
 
-    # Return image if label threshold not met
-    if N > label_threshold:
-        return plot(image)
-
-    max_value = np.amax(comm_matrix)
-
-    # Convert matrix from 2D array to 1D array containg (x, y, volume) values
-    unrolled = []
-    for i in range(N):
-        for j in range(N):
-            unrolled.append((i, j, comm_matrix[i, j]))
-
-    # Generate labels
-    volume_dim = hv.Dimension("volume", value_format=format_size)
-
-    labels = hv.Labels(unrolled, vdims=volume_dim).opts(
-        text_color="volume",
-        color_levels=[0, max_value / 2, max_value],
-        cmap=["black", "white"],
-        text_font_size="9.5pt",
+    p = figure(
+        title="Communication Matrix",
+        x_axis_label="Sender",
+        y_axis_label="Receiver",
+        x_axis_location="above",
+        x_range=[-0.5, N - 0.5],
+        y_range=[N - 0.5, -0.5],
+        #    width=300,
+        height=400,
+        sizing_mode="stretch_width",
     )
+    #    tools=[])
 
-    return plot(image * labels)
+    p.xgrid.visible = False
+    p.ygrid.visible = False
+
+    p.image(
+        image=[np.flip(comm_matrix, 0)],
+        x=-0.5,
+        y=N - 0.5,
+        dw=N,
+        dh=N,
+        palette=YlGnBu9,
+        level="image",
+    )
+    p.grid.grid_line_width = 0.5
+    p.xaxis.ticker = AdaptiveTicker(base=2, min_interval=1)
+    p.yaxis.ticker = AdaptiveTicker(base=2, min_interval=1)
+    p.xaxis.formatter = process_tick_formatter
+    p.yaxis.formatter = process_tick_formatter
+
+    color_bar = ColorBar(
+        color_mapper=mapper,
+        major_label_text_font_size="7px",
+        ticker=BasicTicker(desired_num_ticks=len(YlGnBu9)),
+        formatter=PrintfTickFormatter(format="%d%%"),
+        label_standoff=6,
+        border_line_color=None,
+    )
+    p.add_layout(color_bar, "right")
+
+    show(p)
