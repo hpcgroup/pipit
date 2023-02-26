a272f77875365604da3c67dfa402544c59e6b7c3
Rakrish Dhakal <dhakal.rakrish@gmail.com>
2023-02-26 17:35:43 -0500

    Finish new holoviews implementation

--- pipit/vis/timeline.py
+++ pipit/vis/timeline.py
@@ -1,343 +1,81 @@
-import numpy as np
+import datashader as ds
+import holoviews as hv
 import pandas as pd
-from bokeh.models import (
-    FactorRange,
-    RangeTool,
-    Range1d,
-    WheelZoomTool,
-    ColumnDataSource,
-    CDSView,
-    GroupFilter,
-)
-from bokeh.plotting import figure
-from bokeh.transform import factor_cmap
-from bokeh.events import RangesUpdate
+from holoviews import opts
+from holoviews.operation.datashader import datashade
+from bokeh.themes import Theme
+from ._util import getProcessTickFormatter, getTimeTickFormatter
 
-from ._util import plot, getTimeTickFormatter, format_time
 import pipit as pp
 
-
-def _gen_y(trace):
-    if "y" in trace.events.columns:
-        return
-
-    trace.events["y"] = list(
-        zip(
-            "Process " + trace.events["Process"].astype("str"),
-            "Depth " + trace.events["Depth"].astype("float").fillna(-1).astype("str"),
-        )
-    )
+hv.extension("bokeh")
 
 
 def plot_timeline(trace):
-    """Plots an overflow timeline of events in a Trace.
-
-    Args:
-        trace (pipit.Trace): Trace object whose events are being plotted.
-    """
-
-    # Get all events
-    trace.calc_inc_time()
+    trace._pair_enter_leave()
     trace._gen_calling_relationships()
+    trace.calc_inc_time()
 
-    # Create "y" column based on process and depth
-    # Example value: (Process 1, Depth 3.0)
-    _gen_y(trace)
+    func = trace.events[trace.events["Event Type"] == "Enter"]
 
-    df = trace.events.copy(deep=False)
-    df = df.sort_values(by="Inc Time", ascending=False)
-    df = df.drop(
-        columns=[
-            "Thread",
-            # "Process",
-            "Depth",
-            "Children",
-            "Matching Index",
-            "Attributes",
-            "Parent"
-            # "Inc Time",
-        ]
-    )
-    df["Process"] = df["Process"].astype("float")
-    df["y"] = df["y"].astype("category")
-    df["Timestamp (ns)"] = df["Timestamp (ns)"].astype("float32")
-    df["Matching Timestamp"] = df["Matching Timestamp"].astype("float32")
-    df["Inc Time"] = df["Inc Time"].astype("float32")
-    df["Random"] = np.random.uniform(0.0, 1.0, df.shape[0])
-    ys = df["y"].unique()
+    colors = pp.config["vis"]["colors"]
+    color_key = {
+        cat: tuple(
+            int(x) for x in colors[i].replace("rgb(", "").replace(")", "").split(",")
+        )
+        for i, cat in enumerate(func["Name"].unique())
+    }
+    color_key_ds = {
+        key: tuple(int(x * 0.85) for x in value) for key, value in color_key.items()
+    }
 
-    # Generate comm data for lines
-    sends = df[df["Name"] == "MpiSend"]
-    recvs = df[df["Name"] == "MpiRecv"]
+    rects_df = pd.DataFrame()
+    rects_df["x0"] = func["Timestamp (ns)"]
+    rects_df["y0"] = func["Process"].astype("float") - 0.4
+    rects_df["x1"] = func["Matching Timestamp"]
+    rects_df["y1"] = func["Process"].astype("float") + 0.4
+    rects_df["name"] = func["Name"].astype("category")
 
-    comm = pd.DataFrame()
-    comm["x0"] = sends["Timestamp (ns)"]
-    comm["y0"] = sends["y"]
-    comm["x1"] = recvs["Timestamp (ns)"]
-    comm["y1"] = recvs["y"]
-    comm["cx0"] = comm["x0"] + (comm["x1"] - comm["x0"]) * 0.5
-    comm["cx1"] = comm["x1"] - (comm["x1"] - comm["x0"]) * 0.5
-    comm["dx"] = comm["x1"] - comm["x0"]
-    comm = comm.sort_values(by="dx", ascending=False)
-
-    # Define data source for Bokeh glyphs
-    source = ColumnDataSource(df.head(0))
-    # hist_source = ColumnDataSource(df.head(0))
-    comm_source = ColumnDataSource(comm.head(0))
-
-    # Callback function that updates Bokeh data sources based on current x-range
-    def update_data_sources(event):
-        nonlocal source
-        nonlocal comm_source
-
-        x0 = event.x0 if event is not None else df["Timestamp (ns)"].min()
-        x1 = event.x1 if event is not None else df["Timestamp (ns)"].max()
-
-        N = 700
-        min_time = (x1 - x0) / N
-
-        # Remove events that are out of bounds
-        in_bounds = df[~((df["Matching Timestamp"] < x0) | (df["Timestamp (ns)"] > x1))]
-
-        # Split into large and small events
-        is_large = (in_bounds["Inc Time"] >= min_time) | (in_bounds["Inc Time"].isna())
-        large = in_bounds[is_large]
-        small = in_bounds[~is_large & (in_bounds["Event Type"] == "Enter")]
-
-        # Use reduction operator to reduce small events
-        # Convert small events into bins
-        ls = np.linspace(x0, x1, N + 1)
-        arrs = []
-
-        # For each y-value, generate bins
-        for y in ys:
-            evt = small[small["y"] == y]
-            if evt.shape[0] > 0:
-                out, _ = np.histogram(evt["Timestamp (ns)"], ls, range=(x0, x1))
-                arr = [
-                    {
-                        "Timestamp (ns)": ls[i],
-                        "Matching Timestamp": ls[i + 1],
-                        # "x": (ls[i] + ls[i + 1]) / 2,
-                        # "Event Type": "Bin",
-                        "Event Type": "Enter",
-                        "Count": max(min(x / 10, 2), 0.8),
-                        "y": y,
-                    }
-                    for i, x in enumerate(out)
-                    if x > 0
-                ]
-                arrs += arr
-
-        # Add bins to data source
-        source.data = pd.concat([large, pd.DataFrame(arrs)])
-        # comm_source.data = comm[~((comm["x1"] < x0) | (comm["x0"] > x1))].head(N)
-
-    # Define function color mapping
-    function_cmap = factor_cmap(
-        "Name",
-        palette=pp.config["vis"]["colors"],
-        factors=sorted(df.Name.unique()),
-        end=1,
-        # nan_color="#fff"
-    )
-
-    # Create Bokeh plot
-    p = figure(
-        output_backend="webgl",
-        y_range=FactorRange(*sorted(df["y"].unique(), reverse=True)),
-        sizing_mode="stretch_width",
-        height=min(900, len(df["y"].unique()) * 40 + 30),
-        title="Event Timeline",
-        tools=["xpan", "xwheel_zoom", "hover"],
-        x_axis_location="above",
-        # x_range=[0, df["Timestamp (ns)"].max()]
-    )
-
-    # Add bars for functions
-    hbar_view = CDSView(
-        source=source, filters=[GroupFilter(column_name="Event Type", group="Enter")]
-    )
-    p.hbar(
-        y="y",
-        left="Timestamp (ns)",
-        right="Matching Timestamp",
-        height=1,
-        source=source,
-        view=hbar_view,
-        fill_color=function_cmap,
-        line_color="black",
+    rects_opts = opts.Rectangles(
+        fill_color="name",
         line_width=0.2,
-        fill_alpha=1,
-    )
-
-    # Add dashes for bins
-    # bin_view = CDSView(
-    #     source=source, filters=[GroupFilter(column_name="Event Type", group="Bin")]
-    # )
-    # p.segment(
-    #     x0="x",
-    #     y0=dodge("y", -0.5, range=p.y_range),
-    #     x1="x",
-    #     y1=dodge("y", 0.5, range=p.y_range),
-    #     # angle=1.5708,
-    #     source=source,
-    #     view=bin_view,
-    #     # size=9.5,
-    #     color='black',
-    #     alpha=0.6,
-    #     line_width="Count",
-    # )
-
-    # Add points for instant events
-    # scatter_view = CDSView(
-    #     source=source, filters=[GroupFilter(column_name="Event Type", group="Instant")]
-    # )
-    scatter_source = df[(df["Event Type"] == "Instant")].head(100)
-    p.scatter(
-        x="Timestamp (ns)",
-        y="y",
-        # y=dodge("y", "random", range=p.y_range),
-        source=scatter_source,
-        # view=scatter_view,
-        size=8,
-        alpha=0.5,
-        marker="circle",
-    )
-
-    # Add lines to connect MPI sends and receives
-    p.bezier(
-        x0="x0",
-        y0="y0",
-        x1="x1",
-        y1="y1",
-        cx0="cx0",
-        cy0="y0",
-        cx1="cx1",
-        cy1="y1",
-        source=comm_source,
-        line_width=1,
         line_color="black",
-        alpha=0.6,
+        cmap=color_key,
+        show_legend=False,
     )
 
-    # Additional plot config
-    zoom = p.select(dict(type=WheelZoomTool))
-    p.toolbar.active_scroll = zoom[0]
-    p.x_range.range_padding = 0.2
-    p.xaxis.formatter = getTimeTickFormatter()
-    p.yaxis.group_label_orientation = 0
-    p.yaxis.major_label_text_font_size = "0pt"
-    p.yaxis.major_tick_line_color = None
-    p.yaxis.minor_tick_line_color = None
-    p.ygrid.grid_line_color = None
-    p.on_event(RangesUpdate, update_data_sources)
-    p.lod_threshold = 10000000
-    # p.lod_factor = 3
-
-    update_data_sources(None)
-
-    plot(p)
-
-
-def plot_range_selector(trace):
-    (bins, times) = trace.time_profile(num_bins=1024)
-
-    xs = [(_bin[0]) for _bin in bins]
-
-    functions = trace.events[trace.events["Event Type"] == "Enter"]["Name"].unique()
-
-    for time in times:
-        total_time = sum(time.values())
-        for k, v in time.items():
-            time[k] = v / total_time
-
-        # total_time = sum([ math.exp(x/1000000000) for x in time.values() ])
-        # for k, v in time.items():
-        #     time[k] = math.exp(v/1000000000) / total_time
-
-    data = dict(xs=xs)
-
-    for function in functions:
-        data[function] = np.array([func.get(function, 0) for func in times])
-
-    p = figure(
-        title="Drag the middle and edges of the selection box to change the range",
-        height=400,
-        sizing_mode="stretch_width",
-        toolbar_location=None,
-        y_axis_type=None,
+    copts = dict(
+        responsive=True,
+        height=min(800, max(120, len(func["Process"].unique()) * 60)),
+        default_tools=["xpan", "xwheel_zoom"],
+        active_tools=["xpan", "xwheel_zoom"],
+        xaxis="top",
+        labelled=[],
+        xformatter=getTimeTickFormatter(),
+        yformatter=getProcessTickFormatter(),
+        yticks=len(func["Process"].unique()),
+        show_grid=True,
+        gridstyle=dict(ygrid_line_color=None),
     )
 
-    p.vbar_stack(
-        functions,
-        x="xs",
-        width=bins[0][1] - bins[0][0],
-        color=pp.config["vis"]["colors"][: len(functions)],
-        source=data,
-        legend_label=functions.tolist(),
-    )
+    rects = hv.Rectangles(rects_df, vdims=["name"]).opts(rects_opts).opts(**copts)
 
-    range = Range1d(start=min(xs), end=max(xs))
+    def get_elements(x_range):
+        low, high = (0, rects["x1"].max()) if x_range is None else x_range
+        min_time = (high - low) / 500
 
-    range_tool = RangeTool(x_range=range)
-    range_tool.overlay.fill_color = "navy"
-    range_tool.overlay.fill_alpha = 0.2
+        in_range = rects[~((rects["x1"] < low) | (rects["x0"] > high))]
+        large = in_range[in_range["x1"] - in_range["x0"] >= min_time]
+        small = in_range[in_range["x1"] - in_range["x0"] < min_time]
 
-    p.ygrid.grid_line_color = None
-    p.y_range.start = 0
-    p.xgrid.grid_line_color = None
-    p.axis.minor_tick_line_color = None
-    p.outline_line_color = None
-    p.xaxis.formatter = getTimeTickFormatter()
-    p.add_tools(range_tool)
-    p.toolbar.active_multi = range_tool
-    p.legend.label_text_font_size = "12px"
-    p.legend.spacing = 0
+        small_raster = datashade(
+            small,
+            color_key=color_key_ds,
+            min_alpha=0.1,
+            aggregator=ds.by("name", ds.any()),
+            dynamic=False,
+        )
 
-    p.add_layout(p.legend[0], "right")
+        return large.opts(tools=["hover"]) * small_raster
 
-    plot(p)
-
-
-def plot_time_profile(trace, *args, **kwargs):
-    (bins, times) = trace.time_profile(*args, **kwargs)
-
-    xs = [f"{format(_bin[0])} - {format_time(_bin[1])}" for _bin in bins]
-
-    functions = trace.events[trace.events["Event Type"] == "Enter"]["Name"].unique()
-
-    data = dict(xs=xs)
-
-    for function in functions:
-        data[function] = [func.get(function, 0) for func in times]
-
-    p = figure(
-        x_range=xs,
-        height=400,
-        title="Time Profile",
-        toolbar_location=None,
-        sizing_mode="stretch_width",
-    )
-
-    p.vbar_stack(
-        functions,
-        x="xs",
-        width=0.9,
-        color=pp.config["vis"]["colors"][: len(functions)],
-        source=data,
-        legend_label=functions.tolist(),
-    )
-
-    p.yaxis.formatter = getTimeTickFormatter()
-    p.y_range.start = 0
-    p.xgrid.grid_line_color = None
-    p.axis.minor_tick_line_color = None
-    p.outline_line_color = None
-    p.xaxis.major_label_orientation = 0.3
-    p.legend.label_text_font_size = "12px"
-    p.legend.spacing = 0
-
-    p.add_layout(p.legend[0], "right")
-
-    plot(p)
+    return hv.DynamicMap(get_elements, streams=[hv.streams.RangeX()]).opts(**copts)
