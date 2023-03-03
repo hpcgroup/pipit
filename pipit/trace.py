91bd45ef20841aa1685336ddac103a021c040b35
Aditya Ranjan <adi.k.ranjan@gmail.com>
2023-03-03 18:14:52 -0500

    Trace: Inclusive and Exclusive Time Functions (#44)
    ---------
    Co-authored-by: Abhinav Bhatele <bhatele@cs.umd.edu>

--- pipit/trace.py
+++ pipit/trace.py
@@ -48,97 +48,6 @@ class Trace:
 
         return NsightReader(filename).read()
 
-    def comm_matrix(self, output="size"):
-        """
-        Communication Matrix for Peer-to-Peer (P2P) MPI messages
-
-        Arguments:
-
-        1) output -
-        string to choose whether the communication volume should be measured
-        by bytes transferred between two processes or the number of messages
-        sent (two choices - "size" or "count")
-
-        Returns:
-        Creates three lists - sender ranks, receiver ranks, and message volume.
-        All of these lists are the length of the number of messages sent in the trace.
-        It then loops through these lists containing individual message pairs
-        and volume for those messages and updates the comm matrix.
-
-        Finally, a 2D Numpy Array that represents the communication matrix for all P2P
-        messages of the given trace is returned.
-
-        Note:
-        The first dimension of the returned 2d array
-        is senders and the second dimension is receivers
-        ex) comm_matrix[sender_rank][receiver_rank]
-        """
-
-        # get the list of ranks/processes
-        # (mpi messages are sent between processes)
-        ranks = set(self.events["Process"])
-
-        # create a 2d numpy array that will be returned
-        # at the end of the function
-        communication_matrix = np.zeros(shape=(len(ranks), len(ranks)))
-
-        # filter the dataframe by MPI Send and Isend events
-        sender_dataframe = self.events.loc[
-            self.events["Name"].isin(["MpiSend", "MpiIsend"]),
-            ["Process", "Attributes"],
-        ]
-
-        # get the mpi ranks of all the sender processes
-        # the length of the list is the total number of messages sent
-        sender_ranks = sender_dataframe["Process"].to_list()
-
-        # get the corresponding mpi ranks of the receivers
-        # the length of the list is the total number of messages sent
-        receiver_ranks = (
-            sender_dataframe["Attributes"]
-            .apply(lambda attrDict: attrDict["receiver"])
-            .to_list()
-        )
-
-        # the length of the message_volume list created below
-        # is the total number of messages sent
-
-        # number of bytes communicated for each message sent
-        if output == "size":
-            # (1 communication is a single row in the sender dataframe)
-            message_volume = (
-                sender_dataframe["Attributes"]
-                .apply(lambda attrDict: attrDict["msg_length"])
-                .to_list()
-            )
-        elif output == "count":
-            # 1 message between the pairs of processes
-            # for each row in the sender dataframe
-            message_volume = np.full(len(sender_dataframe), 1)
-
-        for i in range(len(sender_ranks)):
-            """
-            loops through all the communication events and adds the
-            message volume to the corresponding entry of the 2d array
-            using the sender and receiver ranks
-            """
-            communication_matrix[sender_ranks[i], receiver_ranks[i]] += message_volume[
-                i
-            ]
-
-        return communication_matrix
-
-    def message_histogram(self, bins=20, **kwargs):
-        """Generates histogram of message frequency by size."""
-
-        # Filter by send events
-        messages = self.events[self.events["Name"].isin(["MpiSend", "MpiIsend"])]
-
-        # Get message sizes
-        sizes = messages["Attributes"].map(lambda x: x["msg_length"])
-
-        return np.histogram(sizes, bins=bins, **kwargs)
-
     def _match_events(self):
         """Matches corresponding enter/leave events and adds two columns to the
         dataframe: _matching_event and _matching_timestamp
@@ -289,3 +198,132 @@ class Trace:
             self.events = self.events.astype(
                 {"_depth": "category", "_parent": "category"}
             )
+
+    def calc_inc_time(self):
+        # Adds "time.inc" column
+        if "time.inc" not in self.events.columns:
+            if "_matching_timestamp" not in self.events.columns:
+                self._match_events()
+
+            # Uses matching timestamp to calculate the inclusive time
+            self.events.loc[self.events["Event Type"] == "Enter", "time.inc"] = (
+                self.events["_matching_timestamp"] - self.events["Timestamp (ns)"]
+            )
+
+    def calc_exc_time(self):
+        if "time.exc" not in self.events.columns:
+            if "time.inc" not in self.events.columns:
+                self.calc_inc_time()
+            if "_children" not in self.events.columns:
+                self._match_caller_callee()
+
+            # start out with exc times being a copy of inc times
+            exc_times = self.events["time.inc"].to_list()
+            inc_times = self.events["time.inc"].to_list()
+
+            # Filter to events that have children
+            filtered_df = self.events.loc[self.events["_children"].notnull()]
+            parent_df_indices, children = (
+                list(filtered_df.index),
+                filtered_df["_children"].to_list(),
+            )
+
+            # Iterate through the events that are parents
+            for i in range(len(filtered_df)):
+                curr_parent_idx, curr_children = parent_df_indices[i], children[i]
+                for child_idx in curr_children:
+                    # Subtract child's inclusive time to update parent's exclusive time
+                    exc_times[curr_parent_idx] -= inc_times[child_idx]
+
+            self.events["time.exc"] = exc_times
+
+    def comm_matrix(self, output="size"):
+        """
+        Communication Matrix for Peer-to-Peer (P2P) MPI messages
+
+        Arguments:
+
+        1) output -
+        string to choose whether the communication volume should be measured
+        by bytes transferred between two processes or the number of messages
+        sent (two choices - "size" or "count")
+
+        Returns:
+        Creates three lists - sender ranks, receiver ranks, and message volume.
+        All of these lists are the length of the number of messages sent in the trace.
+        It then loops through these lists containing individual message pairs
+        and volume for those messages and updates the comm matrix.
+
+        Finally, a 2D Numpy Array that represents the communication matrix for all P2P
+        messages of the given trace is returned.
+
+        Note:
+        The first dimension of the returned 2d array
+        is senders and the second dimension is receivers
+        ex) comm_matrix[sender_rank][receiver_rank]
+        """
+
+        # get the list of ranks/processes
+        # (mpi messages are sent between processes)
+        ranks = set(self.events["Process"])
+
+        # create a 2d numpy array that will be returned
+        # at the end of the function
+        communication_matrix = np.zeros(shape=(len(ranks), len(ranks)))
+
+        # filter the dataframe by MPI Send and Isend events
+        sender_dataframe = self.events.loc[
+            self.events["Name"].isin(["MpiSend", "MpiIsend"]),
+            ["Process", "Attributes"],
+        ]
+
+        # get the mpi ranks of all the sender processes
+        # the length of the list is the total number of messages sent
+        sender_ranks = sender_dataframe["Process"].to_list()
+
+        # get the corresponding mpi ranks of the receivers
+        # the length of the list is the total number of messages sent
+        receiver_ranks = (
+            sender_dataframe["Attributes"]
+            .apply(lambda attrDict: attrDict["receiver"])
+            .to_list()
+        )
+
+        # the length of the message_volume list created below
+        # is the total number of messages sent
+
+        # number of bytes communicated for each message sent
+        if output == "size":
+            # (1 communication is a single row in the sender dataframe)
+            message_volume = (
+                sender_dataframe["Attributes"]
+                .apply(lambda attrDict: attrDict["msg_length"])
+                .to_list()
+            )
+        elif output == "count":
+            # 1 message between the pairs of processes
+            # for each row in the sender dataframe
+            message_volume = np.full(len(sender_dataframe), 1)
+
+        for i in range(len(sender_ranks)):
+            """
+            loops through all the communication events and adds the
+            message volume to the corresponding entry of the 2d array
+            using the sender and receiver ranks
+            """
+            communication_matrix[sender_ranks[i], receiver_ranks[i]] += message_volume[
+                i
+            ]
+
+        return communication_matrix
+
+    def message_histogram(self, bins=20, **kwargs):
+        """Generates histogram of message frequency by size."""
+
+        # Filter by send events
+        messages = self.events[self.events["Name"].isin(["MpiSend", "MpiIsend"])]
+
+        # Get message sizes
+        sizes = messages["Attributes"].map(lambda x: x["msg_length"])
+
+        return np.histogram(sizes, bins=bins, **kwargs)
