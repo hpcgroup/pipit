542f883550b4df0a5e01fbd856aec8bca96f303b
Abhinav Bhatele <bhatele@cs.umd.edu>
2023-03-13 14:37:02 -0400

    remove pytest import

--- pipit/tests/otf2-tests.py
+++ pipit/tests/otf2-tests.py
@@ -4,7 +4,6 @@
 # SPDX-License-Identifier: MIT
 
 import numpy as np
-import pytest
 from pipit import Trace
 
 
--- pipit/tests/trace.py
+++ pipit/tests/trace.py
@@ -3,7 +3,6 @@
 #
 # SPDX-License-Identifier: MIT
 
-import pytest
 import numpy as np
 from pipit import Trace
 
