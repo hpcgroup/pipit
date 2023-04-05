704fdacbea9f787007128a9e75b66f848817d075
Abhinav Bhatele <bhatele@cs.umd.edu>
2023-04-05 10:57:05 -0400

    partial fix

--- docs/examples/csv_reader.py
+++ docs/examples/csv_reader.py
@@ -6,8 +6,7 @@ import pipit as pp
 if __name__ == "__main__":
     # Use pipit's ``from_otf2`` API to read in the OTF2 traces.
     # The result is stored into pipit's Trace data structure.
-    trace = pp.Trace.from_csv('../../pipit/tests/data/foo-bar.csv')
+    trace = pp.Trace.from_csv("../../pipit/tests/data/foo-bar.csv")
 
+    trace.calc_inc_metrics()
     print(trace.events)
-
-    print(trace.flat_profile())
