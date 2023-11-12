2af70e07dad128f529bd149e93333385e32abf54
hsirkar <dhakal.rakrish@gmail.com>
2023-11-12 13:13:28 -0500

    remove convert function, add comments

--- pipit/writers/chrome_writer.py
+++ pipit/writers/chrome_writer.py
@@ -15,25 +15,36 @@ class ChromeWriter:
         self.trace = trace
         self.filename = filename
 
-    def convert(self):
+    def write(self):
         events = self.trace.events
-
+        
+        # Assign the fields as expected by the Chrome Tracing JSON format
+        # Let's create a new dataframe to avoid modifying the original
         df = pd.DataFrame()
+
+        # "name" represents the event name
         df["name"] = events["Name"]
+
+        # "ph" represents event type -- also called "phase"
+        # Rename Enter events to "B" (begin), Leave events to "E" (end),
+        # and Instant events to "i"
         df["ph"] = events["Event Type"].replace(
             ["Enter", "Leave", "Instant"], ["B", "E", "i"]
         )
+
+        # "ts" represents is the timestamp (in microseconds) of the event
         df["ts"] = (events["Timestamp (ns)"] / 1e3).astype(int)
+
+        # "pid" represents the process ID for the process that the event occurs in
         df["pid"] = events["Process"]
 
+        # "tid" represents the thread ID for the thread that the event occurs in
         if "Thread" in events.columns:
             df["tid"] = events["Thread"]
 
+        # Put all of the additional event attributes into the "args" field
         if "Attributes" in events.columns:
             df["args"] = events["Attributes"]
 
-        return df
-
-    def write(self):
-        df = self.convert()
+        # Write the dataframe to a JSON file
         return df.to_json(path_or_buf=self.filename, orient="records")
