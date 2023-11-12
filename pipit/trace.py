3a47cea41c61f31e27cd0e73792d4dcb7c9ee95f
hsirkar <dhakal.rakrish@gmail.com>
2023-11-12 16:38:09 -0500

    update some stuff

--- pipit/trace.py
+++ pipit/trace.py
@@ -10,6 +10,10 @@ class Trace:
     """A trace dataset is read into an object of this type, which includes one
     or more dataframes.
     """
+    def __init__(self, definitions=None, events=None):
+        self.definitions = definitions
+        self.events = events
+
     @staticmethod
     def from_parquet(path):
         """Read a trace dataset from a parquet file.
@@ -24,5 +28,28 @@ class Trace:
         trace.events = pl.scan_parquet(path)
         return trace
     
+    @staticmethod
+    def from_otf2(path):
+        from pipit.readers.otf2_reader import OTF2Reader
+        trace = OTF2Reader(path).read()
+        trace.events["Process"] = trace.events["Process"].astype("int")
+        trace.events["Thread"] = trace.events["Thread"].astype("int")
+        trace.events = pl.from_pandas(trace.events).lazy()
+        return trace
+    
     def _repr_html_(self):
         return self.events.head().collect()._repr_html_()
+    
+    def _match_events():
+        """
+        
+        In a Polars dataframe containing trace events, the "Event Type" column
+        can be either "Enter", "Leave", or "Instant".
+
+        This function creates a new column, called _matching_event, which
+        matches the indices of "Enter" and "Leave" events. For "Instant" events
+        this column will be null.
+
+        """
+        # stack = []
+        pass
