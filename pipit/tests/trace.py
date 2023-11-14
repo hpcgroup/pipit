297befe79302ef94062090438985b0a6c57d9dd1
Abhinav Bhatele <bhatele@cs.umd.edu>
2023-11-14 16:17:07 -0700

    minor

--- pipit/tests/trace.py
+++ pipit/tests/trace.py
@@ -35,7 +35,7 @@ def test_comm_matrix(data_dir, ping_pong_otf2_trace):
 def test_comm_over_time(data_dir, ping_pong_otf2_trace):
     ping_pong = Trace.from_otf2(str(ping_pong_otf2_trace))
 
-    hist, edges = ping_pong.comm_over_time(output="bytes", message_type="send", bins=5)
+    hist, edges = ping_pong.comm_over_time(output="size", message_type="send", bins=5)
 
     assert len(edges) == 6
     assert all(hist[0:3] == 0)
