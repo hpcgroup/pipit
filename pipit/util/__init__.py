f1d80c01620631e9b67eef0f09f6a4b310f641d7
Abhinav Bhatele <bhatele@cs.umd.edu>
2023-11-14 14:27:16 -0700

    update year

--- pipit/util/__init__.py
+++ pipit/util/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2022-2023 Parallel Software and Systems Group, University of
-# Maryland. See the top-level LICENSE file for details.
+# Copyright 2023 Parallel Software and Systems Group, University of Maryland.
+# See the top-level LICENSE file for details.
 #
 # SPDX-License-Identifier: MIT
