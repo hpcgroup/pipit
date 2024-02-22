4798b247dbb8e2b685bf80bfa4d5732db1bb4444
Rakrish Dhakal <dhakal.rakrish@gmail.com>
2024-02-22 05:53:25 -0500

    rename

--- pipit/data/core.py
+++ pipit/data/core.py
@@ -1,6 +1,7 @@
-# This is based on HoloViews Dataset
-# contains everything from core.py and initial.py
+from abc import 
 
+# This is based on HoloViews Dataset
+# contains everything from query/core.py and query/initial.py
 # https://holoviews.org/reference_manual/holoviews.core.data.html#holoviews.core.data.Dataset
 class Dataset:
     def add_dimension(dimension, dim_val, dim_index):
@@ -12,18 +13,6 @@ class Dataset:
     def apply(funcs):
         pass
 
-    def sum(dimensions):
-        pass
-
-    def mean(dimensions):
-        pass
-
-    def max(dimensions):
-        pass
-
-    def min(dimensions):
-        pass
-
     def array(dimensions):
         pass
 
@@ -33,10 +22,13 @@ class Dataset:
     def columns(dimensions):
         pass
 
+    def dimension_values(dimension, expanded=True):
+        pass
+
     def dimensions(selection):
         pass
 
-    def dimension_values(dimension, expanded=True):
+    def flush():
         pass
 
     def get_dimension(dimension):
@@ -51,28 +43,31 @@ class Dataset:
     def groupby(dimensions, container_type):
         pass
 
-    def hist(dimension, num_bins, bin_range):
-        pass
-
-    def push(event):
-        pass
-
-    def flush():
-        pass
-
-    def ndloc(*args):
-        pass
-
     def head(n):
         pass
 
-    def tail(n):
+    def hist(dimension, num_bins, bin_range):
+        pass
+
+    def max(dimensions):
+        pass
+
+    def mean(dimensions):
+        pass
+
+    def min(dimensions):
+        pass
+
+    def ndloc(*args):
         pass
 
     def options(*args):
         pass
 
     def persist():
+        pass
+
+    def push(event):
         pass
 
     def range(dimension):
@@ -85,6 +80,12 @@ class Dataset:
         pass
 
     def sort(by, reverse):
+        pass
+
+    def sum(dimensions):
+        pass
+
+    def tail(n):
         pass
 
     def __len__():
