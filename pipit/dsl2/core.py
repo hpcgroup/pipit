21cb48440bb30918bd6fab9cdfe0141272e3ec01
Rakrish Dhakal <dhakal.rakrish@gmail.com>
2024-02-24 01:05:34 -0500

    head, tail, show for dsl2

--- pipit/dsl2/core.py
+++ pipit/dsl2/core.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 import pandas as pd
+from pipit.dsl.event import Event
+from tabulate import tabulate
 
-# rename to TraceDataset
+
+# TODO: rename to TraceDataset
 class Dataset:
     def __init__(self, traces=None) -> None:
         self.traces = traces if traces is not None else dict()
@@ -23,7 +26,7 @@ class Dataset:
         rank = event.rank
 
         if rank not in self.traces:
-            self.traces[rank] = _Trace()
+            self.traces[rank] = _Trace(rank=rank)
 
         self.traces[rank].push_event(event)
 
@@ -31,8 +34,68 @@ class Dataset:
         for rank in self.traces:
             self.traces[rank].flush()
 
+    def head(self, n=5) -> Dataset:
+        traces = {rank: trace.head(n=n) for rank, trace in self.traces.items()}
+        events = [event for trace in traces.values() for event in trace.collect()]
+
+        events.sort(key=lambda event: event.timestamp)
+
+        traces = {}
+        for event in events[:n]:
+            rank = event.rank
+            if rank not in traces:
+                traces[rank] = _Trace(rank=rank)
+            traces[rank].push_event(event)
+
+        for rank in traces:
+            traces[rank].flush()
+
+        return Dataset(traces)
+
+    def tail(self, n=5) -> Dataset:
+        traces = {rank: trace.tail(n=n) for rank, trace in self.traces.items()}
+        events = [event for trace in traces.values() for event in trace.collect()]
+
+        events.sort(key=lambda event: event.timestamp)
+
+        traces = {}
+        for event in events[-n:]:
+            rank = event.rank
+            if rank not in traces:
+                traces[rank] = _Trace(rank=rank)
+            traces[rank].push_event(event)
+
+        for rank in traces:
+            traces[rank].flush()
+
+        return Dataset(traces)
+
     def show(self) -> None:
-        print(self.__repr__())
+        if len:
+            if len(self) > 20:
+                top = [e.to_dict() for e in self.head().collect()]
+                middle = {k: "..." for k in top[0].keys()}
+                bottom = [e.to_dict() for e in self.tail().collect()]
+
+                print(
+                    tabulate(top + [middle] + bottom, headers="keys", tablefmt="psql")
+                )
+            else:
+                print(
+                    tabulate(
+                        [e.to_dict() for e in self.collect()],
+                        headers="keys",
+                        tablefmt="psql",
+                    )
+                )
+
+        print(self.__str__())
+
+    def collect(self):
+        events = {rank: trace.collect() for rank, trace in self.traces.items()}
+        tmp = [event for events in events.values() for event in events]
+        tmp.sort(key=lambda event: event.timestamp)
+        return tmp
 
     def filter(self, condition) -> Dataset:
         filtered_traces = {
@@ -45,8 +108,9 @@ class Dataset:
 
 
 class _Trace:
-    def __init__(self, data=None) -> None:
+    def __init__(self, data=None, rank=0) -> None:
         self.data = data if data is not None else pd.DataFrame()
+        self.rank = rank
         self.buffer = []
 
     def __len__(self) -> int:
@@ -80,8 +144,40 @@ class _Trace:
         self.data = pd.concat([self.data, df])
         self.buffer = []
 
+    def head(self, n=5) -> _Trace:
+        df = self.data.head(n=n)
+        return _Trace(df, rank=self.rank)
+
+    def tail(self, n=5) -> _Trace:
+        df = self.data.tail(n=n)
+        return _Trace(df, rank=self.rank)
+
+    def collect(self):
+        records = self.data.reset_index().to_dict(orient="records")
+        events = [Event(rank=self.rank, **record) for record in records]
+        return events
+
     def show(self) -> None:
-        pass
+        if len(self):
+            if len(self) > 20:
+                top_df = self.head().data.reset_index()
+                bottom_df = self.tail().data.reset_index()
+
+                top = top_df.to_dict(orient="records")
+                middle = {k: "..." for k in top_df.columns}
+                bottom = bottom_df.to_dict(orient="records")
+
+                print(
+                    tabulate(top + [middle] + bottom, headers="keys", tablefmt="psql")
+                )
+            else:
+                print(
+                    tabulate(
+                        self.data, headers="keys", tablefmt="psql", showindex=False
+                    )
+                )
+
+        print(self.__str__())
 
     def filter(self, condition) -> _Trace:
-        return _Trace(self.data.query(condition))
+        return _Trace(self.data.query(condition), rank=self.rank)
