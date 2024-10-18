622242bb61ba7568a3df34c9a1b3d760c9abd7cc
zoreoo <122127038+zoreoo@users.noreply.github.com>
2024-10-17 22:22:38 -0400

    Change match_charm_messages to single-loop (and fix a bug)

--- pipit/trace.py
+++ pipit/trace.py
@@ -202,40 +202,29 @@ class Trace:
         matching_events = list(self.events["_matching_event"])
         matching_times = list(self.events["_matching_timestamp"])
 
-        charm_send_events = self.events[self.events["Attributes"].apply(
-            lambda x: False if not x else ("Entry Type" in  x) and (x["Entry Type"] == "Create")
+        charm_events = self.events[self.events["Attributes"].apply(
+            lambda x: False if not x else ("Entry Type" in x) and (x["Entry Type"] == "Processing" 
+                                                                   or x["Entry Type"] == "Create")
         )]
 
-        charm_receive_events = self.events[self.events["Attributes"].apply(
-            lambda x: False if not x else ("Entry Type" in x) and (x["Entry Type"] == "Processing")
-        )]
+        df_indices = list(charm_events.index)
+        timestamps = list(charm_events["Timestamp (ns)"])
+        attrs = list(charm_events["Attributes"])
 
-        df_indices = list(charm_send_events.index)
-        timestamps = list(charm_send_events["Timestamp (ns)"])
-        attrs = list(charm_send_events["Attributes"])
-
+        # queue is indexed by event ID and will store (dataframe index, timestamp) from
+        # message send events
         queue = {}
 
-        # Add all necessary send event information to the list
-        for i in range(len(charm_send_events)):
+        # Iterate through send/receive events
+        for i in range(len(charm_events)):
             curr_df_index = df_indices[i]
             curr_timestamp = timestamps[i]
+            curr_event_type = attrs[i]["Entry Type"]
             curr_event_id = attrs[i]["Event ID"]
 
-            queue[curr_event_id] = (curr_df_index, curr_timestamp)
-        
-
-        df_indices = list(charm_receive_events.index)
-        timestamps = list(charm_receive_events["Timestamp (ns)"])
-        attrs = list(charm_receive_events["Attributes"])
-
-        for i in range(len(charm_receive_events)):
-            curr_df_index = df_indices[i]
-            curr_timestamp = timestamps[i]
-            curr_event_id = attrs[i]["Event ID"]
-
-            if curr_event_id in queue:
-    
+            if curr_event_type == "Create":
+                queue[curr_event_id] = (curr_df_index, curr_timestamp)
+            elif curr_event_type == "Processing" and curr_event_id in queue:
                 send_df_index = queue[curr_event_id][0]
                 send_timestamp = queue[curr_event_id][1]
 
@@ -244,7 +233,8 @@ class Trace:
 
                 matching_times[send_df_index] = curr_timestamp
                 matching_times[curr_df_index] = send_timestamp
-
+                
+        
         self.events["_matching_event"] = matching_events
         self.events["_matching_timestamp"] = matching_times
 
